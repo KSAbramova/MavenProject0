f14c533c874667686e087d746e516498867c6035
KSAbramova <263575666+KSAbramova@users.noreply.github.com>
2026-03-09 16:11:15 +0300

    Create class Person.java
    Создан класс-сущность Person

---
+++ src/main/java/Person.java
@@ -0,0 +1,22 @@
+public class Person {
+    private final String name;
+    private final String address;
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public String toString() {
+        return "Имя: " + name + "\nАдрес: " + address;
+    }
+}
